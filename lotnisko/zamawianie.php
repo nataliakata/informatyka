46362e92e0ca8bffd2ce5d021fd2e8151bf0428d
nataliakata <60201666+nataliakata@users.noreply.github.com>
2020-09-22 21:02:29 +0200

    Add files via upload

---
+++ lotnisko/zamawianie.php
@@ -0,0 +1,8 @@
+<?php
+$miasto1=$_GET['miasto1'];
+$miasto1=$_GET['miasto2'];
+date_default_timezone_set("Poland/Warsaw");
+
+echo "Odległość wynosi ".$odl=50." km.";
+echo "Koszt w jedną stronę na dzień ".date(l.d.m.Y).", godzina ".date(H:i)." ".$cena=$odl*2.08."zl.";
+?>
